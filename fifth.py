da33085f103b66153d5dcdf1fd285bc570efc88c
masterscode <ogbinakaemmanuel@gmail.com>
2020-02-23 17:04:32 +0100

    end of python crash course, thanks brad traversy

--- fifth.py
+++ fifth.py
@@ -1,4 +1,20 @@
 import datetime as Time
 
 # import datetime as date
-print(dir(Time))
+# print(dir(Time))
+
+#try continue statement
+
+listings = ['amen', 'jane', 'algo', 'patience']
+
+
+'''
+for item in listings:
+    if item == 'algo': continue
+    print(f'the total number of items in listings are {len(listings)}, and the items in listings are: {item}')
+'''
+
+from datetime import date   
+datelistings = dir(date)
+for d in datelistings:
+    print(d)
