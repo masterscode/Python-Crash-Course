6fcd9c517f09366dc3fdec1580582d1b34687a67
masterscode <ogbinakaemmanuel@gmail.com>
2020-02-23 15:35:34 +0100

    add all this time

---
+++ fifth.py
@@ -0,0 +1,4 @@
+import datetime as Time
+
+# import datetime as date
+print(dir(Time))
---
+++ fourth.py
@@ -0,0 +1,22 @@
+#using dictionaries
+#a dictionary is a collection which is unordered, changeable and indexed. no duplicate memebers
+
+#create dictionary
+person = {
+    'first key': 'first value',
+    'second key': 'second value'
+}
+
+# iterator = iter(person)
+# print(next(iterator))
+# print(next(iterator))
+
+
+# import first as firstModule
+# from first import name
+
+# print (name)
+
+import platform
+
+print( dir(platform.machine))
