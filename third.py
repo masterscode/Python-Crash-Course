9f93e86d8a96e068ad244848912b72cb4bffc61b
masterscode <ogbinakaemmanuel@gmail.com>
2020-02-23 02:52:39 +0100

    with love from the night

---
+++ third.py
@@ -0,0 +1,14 @@
+# a tuple is a collection which is ordered and unchangeable. allows duplicate members
+'''create a tuple'''
+
+fruits = ('mangoes', 'grapes', 'oranges')
+anotherFruits = tuple(('mangoes', 'grapes', 'oranges'))
+
+
+
+
+
+
+
+
+#a set is a collection which is unordered and unindexed. no duplicate members
